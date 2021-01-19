3bee91185d6c849aae81232958a3a1add09bfe9a
Jordan Tranchina <Jordan12522@gmail.com>
2021-01-19 16:29:42 -0600

    finished lab!

--- lab/week3.js
+++ lab/week3.js
@@ -1,8 +1,8 @@
-let numberOfProducts        // expects a Number 
-let products                // expects an Array of product data
-let promotion1Description   // expects a String describing the promotion
-let promotion2Description   // expects a String describing the promotion
-let promotion3Description   // expects a String describing the promotion
+let numberOfProducts        // expects a Number ✅
+let products                // expects an Array of product data✅
+let promotion1Description   // expects a String describing the promotion ✅
+let promotion2Description   // expects a String describing the promotion✅
+let promotion3Description   // expects a String describing the promotion✅
 let promotion1Icon          // expects a String (emoji)
 let promotion2Icon          // expects a String (emoji)
 let promotion3Icon          // expects a String (emoji)
@@ -29,6 +29,55 @@ function pageLoad() {
 
   // 🔥🔥🔥 YOUR CODE GOES HERE 🔥🔥🔥
   console.log(database)
+  numberOfProducts = database.products.length
+  products = database.products
+
+  // setting promotions
+  promotion1 = database.promotions[Math.floor(Math.random() * database.promotions.length)]
+  promotion2 = database.promotions[Math.floor(Math.random() * database.promotions.length)]
+  promotion3 = database.promotions[Math.floor(Math.random() * database.promotions.length)]
+
+  //setting promotion descriptions
+  promotion1Description = promotion1.description
+  promotion2Description = promotion2.description
+  promotion3Description = promotion3.description
+
+  //setting promotion descriptions
+
+
+  //pairing icons with promotion types - Promotion 1
+  if (promotion1.type == "shipping") {
+    promotion1Icon = "🚛"
+
+  } if (promotion1.type == "discount") {
+    promotion1Icon = "🤑"
+
+  } if (promotion1.type == "contest") {
+    promotion1Icon = "🎰"
+  }
+
+  //pairing icons with promotion types - Promotion 2
+  if (promotion2.type == "shipping") {
+    promotion2Icon = "🚛"
+
+  } if (promotion2.type == "discount") {
+    promotion2Icon = "🤑"
+
+  } if (promotion2.type == "contest") {
+    promotion2Icon = "🎰"
+  }
+
+  //pairing icons with promotion types - Promotion 3
+  if (promotion3.type == "shipping") {
+    promotion3Icon = "🚛"
+
+  } if (promotion3.type == "discount") {
+    promotion3Icon = "🤑"
+
+  } if (promotion3.type == "contest") {
+    promotion3Icon = "🎰"
+  }
+
 
   //  👆 YOUR CODE ENDS HERE. DON'T CHANGE ANY OTHER CODE 👆
 }
